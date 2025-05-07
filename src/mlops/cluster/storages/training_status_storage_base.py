cb231265f1f3a4d6dadba4d4e90ffb3cd1ec6d1c
susautw <susautw@gmail.com>
2025-05-07 17:37:36 +0000

    Refactor TrainingStatusStorageBase to include abstract methods for get, save, and delete operations

--- src/mlops/cluster/storages/training_status_storage_base.py
+++ src/mlops/cluster/storages/training_status_storage_base.py
@@ -1,4 +1,6 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from mlops.common.model import TrainingStatus
 
 
 class TrainingStatusStorageBase(ABC):
@@ -8,3 +10,29 @@ class TrainingStatusStorageBase(ABC):
     Training status storage classes are used to store, cache and manage training status
     information in a cluster.
     """
+
+    @abstractmethod
+    def get(self, task_id: str) -> TrainingStatus | None:
+        """
+        Get training status by task id
+
+        :param task_id: id of the training task
+        :return: TrainingStatus object if exists, None otherwise
+        """
+
+    @abstractmethod
+    def save(self, training_status: TrainingStatus) -> None:
+        """
+        Save training status
+
+        :param training_status: TrainingStatus object
+        """
+
+    @abstractmethod
+    def delete(self, task_id: str) -> bool:
+        """
+        Delete training status by task id
+
+        :param task_id: id of the training task
+        :return: True if training status was deleted, False otherwise
+        """
